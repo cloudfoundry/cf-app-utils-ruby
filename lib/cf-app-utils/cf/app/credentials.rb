d4a41317ac23670debaf05408fb22a8c99d5d8e8
Adam Berlin <berlin.ab@gmail.com>
2015-07-24 00:27:01 -0400

    Allow consumers to inject ENV

--- lib/cf-app-utils/cf/app/credentials.rb
+++ lib/cf-app-utils/cf/app/credentials.rb
@@ -1,20 +1,26 @@
 module CF::App
   class Credentials
-    class << self
+
+      attr_reader :service_service
+
+      def initialize(env)
+          @service_service = Service.new(env)
+      end
+
       # Returns credentials for the service instance with the given +name+.
       def find_by_service_name(name)
-        service = Service.find_by_name(name)
+        service = service_service.find_by_name(name)
         service['credentials'] if service
       end
 
       # Returns credentials for the first service instance with the given +tag+.
       def find_by_service_tag(tag)
-        service = Service.find_by_tag(tag)
+        service = service_service.find_by_tag(tag)
         service['credentials'] if service
       end
 
       def find_all_by_service_tag(tag)
-        services = Service.find_all_by_tag(tag)
+        services = service_service.find_all_by_tag(tag)
         services.map do |service|
           service['credentials']
         end
@@ -24,22 +30,25 @@ module CF::App
       def find_all_by_all_service_tags(tags)
         return [] if tags.empty?
 
-        Service.find_all_by_tags(tags).map { |service| service['credentials'] }
+        service_service.find_all_by_tags(tags).map { |service| service['credentials'] }
       end
 
       # Returns credentials for the first service instance with the given +label+.
       def find_by_service_label(label)
-        service = Service.find_by_label(label)
+        service = service_service.find_by_label(label)
         service['credentials'] if service
       end
 
       # Returns credentials for all service instances with the given +label+.
       def find_all_by_service_label(label)
-        services = Service.find_all_by_label(label)
+        services = service_service.find_all_by_label(label)
         services.map do |service|
           service['credentials']
         end
       end
-    end
+
+      def self.method_missing(message, *args, &block)
+          Credentials.new(ENV).public_send(message, *args, &block)
+      end
   end
 end
